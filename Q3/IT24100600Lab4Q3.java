a409949debea9df081ee85c555cbd0dbad32ff18
IT24100600 <it24100600@my.sliit.lk>
2024-08-20 23:17:33 +0530

    Update IT24100600Lab4Q3.java

--- Q3/IT24100600Lab4Q3.java
+++ Q3/IT24100600Lab4Q3.java
@@ -8,14 +8,7 @@ public class IT24100600Lab4Q3
 		System.out.print("Enter a number : ");
 		double num1 = keyboard.nextDouble();
 		
-		/*if (num1>0)
-				{System.out.println("The number is Positive");}
-		else if (num1<0)
-				{System.out.println("The number is Negative");}
-		else
-				{System.out.println("The number is Zero");}*/
-				
-				
+						
 		String result = (num1 > 0) ? "The number is Positive" : (num1 < 0) ? "The number is Negative" : "The number is Zero";
 		System.out.println(result);
 	}
